2011e4f18edd534a65024039fd13cf8f69340786
Topandas69 <W0790712@myscc.ca>
2023-02-11 03:58:00 -0500

    pipe filtering type

--- Top_Das_MyFavouriteAnimeSeries/src/app/content-type.pipe.ts
+++ Top_Das_MyFavouriteAnimeSeries/src/app/content-type.pipe.ts
@@ -1,12 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Content } from './helper-files/content-interface';
 
 @Pipe({
   name: 'contentType'
 })
 export class ContentTypePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(values: Content[], typeFilter?: string): Content[] {
+    if (!typeFilter){
+      return values.filter(value => !value.type);
+    }
+    return values.filter(value => value.type === typeFilter);
   }
 
 }
