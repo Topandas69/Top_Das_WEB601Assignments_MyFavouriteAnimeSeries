b73a6d80296f23450665c19eb3d8c3d4f1f86588
Topandas69 <W0790712@myscc.ca>
2023-03-09 18:16:27 -0500

    Service functioning

--- Top_Das_MyFavouriteAnimeSeries/src/app/services/anime-series.service.ts
+++ Top_Das_MyFavouriteAnimeSeries/src/app/services/anime-series.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Content } from '../helper-files/content-interface';
+import { contentsArray } from '../helper-files/contentDb';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,8 @@ import { Injectable } from '@angular/core';
 export class AnimeSeriesService {
 
   constructor() { }
+
+  getContent(): Observable<Content[]>{
+    return of(contentsArray);
+  } 
 }
