d362bc34f699b681aaf9c7134625ef534ec058c1
Topandas69 <W0790712@myscc.ca>
2023-03-09 19:34:15 -0500

    added a top content

--- Top_Das_MyFavouriteAnimeSeries/src/app/app.component.ts
+++ Top_Das_MyFavouriteAnimeSeries/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AnimeSeriesService } from './services/anime-series.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Top_Das_MyFavouriteAnimeSeries';
+  topContent: any = {};
+
+  
+  constructor(private AnimeSeriesService: AnimeSeriesService){}
+
+  logId(){
+    console.log(`ID: ${this.topContent.id}`);
+    console.log(`Title: ${this.topContent.title}`);
+  }
+
+  ngOnInit(){
+    const content = this.AnimeSeriesService.getContentById(7).subscribe(content => this.topContent = content);
+  }
 }
